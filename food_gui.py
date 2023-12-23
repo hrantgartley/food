c4679ea24fbc81fddcefe5f1a885cac359462855
hrantgartley <doyouevenjump@gmail.com>
2023-12-22 22:53:23 -0600

    changes

--- food_gui.py
+++ food_gui.py
@@ -8,8 +8,13 @@ from PyQt5.QtGui import QIcon
 class WeekdayFood:
     def __init__(self):
         self.choice = [
-            'Chick-fil-A', 'Moe\'s', 'Panda Express',
-            'Cafeteria', 'Burger 256', 'CREATE', 'Lions Corner'
+            "Chick-fil-A",
+            "Moe's",
+            "Panda Express",
+            "Cafeteria",
+            "Burger 256",
+            "CREATE",
+            "Lions Corner",
         ]
         self.rand_index = random.randint(0, len(self.choice) - 1)
 
@@ -18,23 +23,21 @@ class WeekdayFood:
         return weekday_food
 
     def __str__(self):
-        return f'Weekday food: {self.get_food()}'
+        return f"Weekday food: {self.get_food()}"
 
 
 class WeekendFood(WeekdayFood):
     def __init__(self):
-        self.choice = [
-            'Chick-fil-A', 'Cafeteria', 'CREATE', 'Starbucks'
-        ]
+        self.choice = ["Chick-fil-A", "Cafeteria", "CREATE", "Starbucks"]
         self.rand_index = random.randint(0, len(self.choice) - 1)
 
     def __str__(self):
-        return f'Weekend food: {self.get_food()}'
+        return f"Weekend food: {self.get_food()}"
 
 
 class HomeFood(WeekdayFood):
     def __init__(self):
-        with open('food.txt', 'r') as f:
+        with open("food.txt", "r") as f:
             self.choice = [line.splitlines() for line in f.readlines()]
         self.rand_index = random.randint(0, len(self.choice) - 1)
 
@@ -43,11 +46,9 @@ class HomeFood(WeekdayFood):
         return f'Home food: {", ".join(food_item)}'
 
 
-
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setGeometry(100, 100, 400, 200)
         self.setWindowTitle("Food Picker for UNA")
         self.label = QLabel(self)
@@ -67,17 +68,20 @@ class MyWindow(QMainWindow):
             home_food = HomeFood()
             self.label.setText(str(home_food))
         # checks if it's a weekday and if the month is between august and april
-        elif datetime.today().weekday() < 5 and current_month >= 8 and current_month <= 4:
+        elif (
+            datetime.today().weekday() < 5 and current_month >= 8 and current_month <= 4
+        ):
             weekday_food = WeekdayFood()
             self.label.setText(str(weekday_food))
-        # checks if it's a weekend and if the month is between august and april
+        # it's a weekend and if the month is between august and april
         else:
             weekend_food = WeekendFood()
             self.label.setText(str(weekend_food))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
-    window.setWindowIcon(QIcon('meal-food-icon.png'))
+    window.setWindowIcon(QIcon("meal-food-icon.png"))
     window.show()
     sys.exit(app.exec_())
