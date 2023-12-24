98a45d1fcda3a38f13d316a0cdc0c6b6ef618ac1
hrantgartley <doyouevenjump@gmail.com>
2023-12-24 00:17:58 -0600

    pycache to gitignore and add a main function to main.py

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from food_gui import MyWindow
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
-if __name__ == "__main__":
+
+def main():
     app = QApplication(sys.argv)
     window = MyWindow()
     window.setWindowIcon(QIcon("meal-food-icon.png"))
     window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
